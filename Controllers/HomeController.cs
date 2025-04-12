0564ecaeb9406df40c3a6b1fac12525a1a7c0361
Tanmay <tanmayjadhav506@gmail.com>
2025-04-12 22:19:08 +0530

    initial commit of ASP.NET Core Mvc project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SelfPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "My description page.";
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "My contact page.";
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/MyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SelfPortfolio2.Controllers
+{
+    public class MyController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public MyController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public IActionResult GetUserName()
+        {
+            // Ensure HttpContext and User.Identity are not null before accessing Name
+            var httpContext = _httpContextAccessor.HttpContext;
+            if (httpContext?.User?.Identity?.Name != null)
+            {
+                return Ok(httpContext.User.Identity.Name);  // Return the name as a JSON response
+            }
+
+            // Return a 404 Not Found if the user name is not available
+            return NotFound("User name not available.");
+        }
+    }
+}
